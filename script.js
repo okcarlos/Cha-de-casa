5af68cb541774c57cf36e9249672e9f294e0aa19
okcarlos <okcarlos122@gmail.com>
2025-09-14 18:38:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ signInAnonymously(auth)
       snapshot.forEach((docSnap) => {
         const produto = docSnap.data();
         const li = document.createElement("li");
-        li.style.fontSize = "100px"
+        li.style.fontSize = "60px"
 
         let texto = produto.nome;
         if (produto.reservado && produto.reservadoPor) {
