08cfbc39bab688b5e0246fca0c5e059e3e104a94
okcarlos <okcarlos122@gmail.com>
2026-02-14 18:29:49 -0300

    Change button height from 40px to 10px

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ const lista = document.getElementById("lista");
     botao.style.textAlign = "center"
     botao.style.fontSize = "25px"
     botao.style.width = "140px"
-    botao.style.height = "40px"
+    botao.style.height = "10px"
     botao.onclick = () =>{
       tutorial.remove()
     }
